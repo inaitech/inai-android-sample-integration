424af8211a99e871d04b94b89ff9aca33e032de3
inai-chinmay-flexiple <chinmay.flexiple@inai.io>
2022-07-28 23:33:24 +0530

    Added Config Object

---
+++ app/src/main/java/io/inai/android_sample_integration/Config.kt
@@ -0,0 +1,9 @@
+package io.inai.android_sample_integration
+
+object Config {
+
+    var inaiToken = "<<token>>"
+    var inaiPassword = "<<password>>"
+    var countryCode = "<<countryCode>>"
+    var currency = "<<currencyCode>>"
+}
