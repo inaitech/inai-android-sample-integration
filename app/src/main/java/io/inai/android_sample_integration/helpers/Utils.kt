6e77b6dc35f8a6915e0a7745bcba0a268ca088b7
inai-chinmay-flexiple <103988822+inai-chinmay-flexiple@users.noreply.github.com>
2022-08-12 13:48:56 +0530

    Prd 4643 android sample app headless validate fields (#5)
    * Implemented helper class ValidateFieldsHelper.ktfor validating fields.
    
    * Implemented GooglePayActivity.kt
    Changed data classes.
    Added new alert extension function for Activity.
    
    * Added progress indicator
    
    * Moved customerId variable to config file.
    
    * Added dropdown list selection feature for countries in form fields.
    
    * Code refactoring

--- app/src/main/java/io/inai/android_sample_integration/helpers/Utils.kt
+++ app/src/main/java/io/inai/android_sample_integration/helpers/Utils.kt
@@ -1,8 +1,12 @@
+@file:Suppress("UNUSED_ANONYMOUS_PARAMETER")
+
 package io.inai.android_sample_integration.helpers
 
+import android.app.Activity
 import android.content.Context
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import kotlinx.serialization.json.Json
 
@@ -43,6 +47,21 @@ fun Fragment.showAlert(message: String) {
         "Ok"
     ) { dialog, _ ->
         findNavController().navigateUp()
+    }
+
+    val alertDialog: AlertDialog = alertBuilder.create()
+    alertDialog.show()
+}
+
+fun Activity.showAlert(message: String) {
+    val alertBuilder: AlertDialog.Builder = AlertDialog.Builder(this)
+    alertBuilder.setMessage(message)
+    alertBuilder.setCancelable(true)
+
+    alertBuilder.setPositiveButton(
+        "Ok"
+    ) { dialog, _ ->
+        finish()
     }
 
     val alertDialog: AlertDialog = alertBuilder.create()
