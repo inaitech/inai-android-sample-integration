d95311fc4facc293a5622440d57a689d368a0f8e
inai-chinmay-flexiple <103988822+inai-chinmay-flexiple@users.noreply.github.com>
2022-08-17 13:01:11 +0530

    Prd 4720 android sample app refactor features into individual views (#7)
    * Removed Card Info and Validate Fields API from Make Payment
    
    * Local Validations implemented
    
    * Validate Fields Flow Implemented
    
    * Changed class Names by omitting term Fragment at end.
    Implemented MakePaymentHelper class.
    Implemented SavePayment method flow.
    
    * Refactored ValidateFieldsPaymentOptions.kt class
    
    * Separated Make payments and Pay With Saved Payment Methods.
    Implemented separate flow for Pay With Saved Payment Methods
    
    * Implemented get card info flow.
    Removed Card Info Helper class
    
    * Local validations bugs fixed
    
    * Refactored Network Request Handler to implement cancellation of coroutine scope.
    
    * Fixed ID issue in Pay With Saved Payment Method Flow.
    
    * Activity titles fixed
    
    * Added Fragment lifecycle callbacks to cancel network requests on screen exit.
    
    * UI issue fixes.
    
    * Moved independent files to respective packages.
    Removed onCreate method call in fragments and passed layout ids in constructors.
    
    * Removed slash from base URLs and added to individual end points.
    
    * Implemented Google Pay Flow
    
    * Added Parcelable implementation to data classes
    
    * Added Parcelable implementation to data classes
    Updated README.md file
    
    * Bug Fixes.
    
    * Code Refactoring for Make Payment
    
    * Code Refactoring for Save A Payment Method and Validate Fields Flow
    
    * Code refactoring for Pay With Saved Payment Methods
    
    * Code refactoring for Google Pay
    
    * Code refactoring complete
    
    * Removed unused functions in NetworkRequestHandler.kt
    
    * Removed hard coded values in config and Log statements
    
    Co-authored-by: Chinmay Kalmane <chinmay.flexiple@inai.io>

---
+++ app/src/main/java/io/inai/android_sample_integration/google_pay/PaymentOptionsDataModel.kt
@@ -0,0 +1,99 @@
+package io.inai.android_sample_integration.google_pay
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
+
+
+/**
+ * Data classes related to Payment Options
+ */
+@Serializable
+data class PaymentOptionsResult(
+    @SerialName("payment_method_options")
+    val paymentMethodOptions: List<PaymentMethodOption>?
+)
+
+@Serializable
+@Parcelize
+data class PaymentMethodOption(
+    @SerialName("configs")
+    val configs: @RawValue JsonElement? = null,
+    @SerialName("form_fields")
+    val formFields: @RawValue List<FormField>,
+    @SerialName("rail_code")
+    val railCode: String,
+) : Parcelable
+
+@Serializable
+@Parcelize
+data class FormField(
+    @SerialName("field_type")
+    val fieldType: String,
+    @SerialName("label")
+    val label: String,
+    @SerialName("name")
+    val name: String,
+    @SerialName("placeholder")
+    val placeholder: String? = null,
+    @SerialName("required")
+    val required: Boolean,
+    @SerialName("validations")
+    val validations: Validations,
+    @SerialName("data")
+    val data: Data? = null
+) : Parcelable
+
+@Serializable
+@Parcelize
+data class SupportedNetwork(
+    @SerialName("name")
+    val name: String? = null
+) : Parcelable
+
+@Parcelize
+@Serializable
+data class Tokenization(
+    @SerialName("parameters")
+    val parameters: Parameters? = null,
+    @SerialName("type")
+    val type: String? = null
+) : Parcelable
+
+@Serializable
+@Parcelize
+data class Parameters(
+    @SerialName("protocolVersion")
+    val protocolVersion: String? = null,
+    @SerialName("publicKey")
+    val publicKey: String? = null
+) : Parcelable
+
+@Serializable
+@Parcelize
+data class Validations(
+    var input_mask_regex: String? = null,
+    var max_length: Int? = null,
+    var min_length: Int? = null,
+    var pattern: String? = null,
+    var required: Boolean? = null
+) : Parcelable
+
+@Serializable
+@Parcelize
+data class Data(
+    @SerialName("values")
+    var values: List<Values>? = null
+) : Parcelable
+
+@Serializable
+@Parcelize
+data class Values(
+    val label: String,
+    val value: String
+) : Parcelable
+
+
