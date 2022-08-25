b95acd3de5ae172211f59d7632f923d476675a4a
Chinmay Kalmane <chinmay.flexiple@inai.io>
2022-08-25 14:29:35 +0530

    Added implementation for order creation and drop in checkout in Drop In flow.

--- app/src/main/java/io/inai/android_sample_integration/MainActivity.kt
+++ app/src/main/java/io/inai/android_sample_integration/MainActivity.kt
@@ -4,20 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import io.inai.android_sample_integration.drop_in.DropInActivity
 import io.inai.android_sample_integration.headless.HeadlessActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var headless : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //  Remove Title bar
         supportActionBar?.title = "Android Sample Integration"
-        headless = findViewById<Button>(R.id.btn_headless)
 
-        headless.setOnClickListener {
+        btn_headless.setOnClickListener {
             val intent = Intent(this,HeadlessActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_dropin.setOnClickListener {
+            val intent = Intent(this,DropInActivity::class.java)
             startActivity(intent)
         }
     }
