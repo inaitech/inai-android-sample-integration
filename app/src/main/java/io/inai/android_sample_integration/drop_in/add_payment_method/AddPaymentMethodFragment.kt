79c19f2f3c07f53f10c9c088c6550576a07e8e03
Chinmay Kalmane <chinmay.flexiple@inai.io>
2022-11-11 16:28:06 +0530

    Add Payment Method Interface implemented.

---
+++ app/src/main/java/io/inai/android_sample_integration/drop_in/add_payment_method/AddPaymentMethodFragment.kt
@@ -0,0 +1,154 @@
+package io.inai.android_sample_integration.drop_in.add_payment_method
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.View
+import io.inai.android_sample_integration.BuildConfig
+import io.inai.android_sample_integration.Config
+import io.inai.android_sample_integration.R
+import io.inai.android_sample_integration.helpers.NetworkRequestHandler
+import io.inai.android_sample_integration.helpers.json
+import io.inai.android_sample_integration.helpers.showAlert
+import io.inai.android_sdk.*
+import kotlinx.android.synthetic.main.fragment_present_checkout.btn_buy
+import kotlinx.android.synthetic.main.fragment_present_checkout.progressBar
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+
+class AddPaymentMethodFragment : Fragment(R.layout.fragment_add_payment_method), InaiPaymentMethodDelegate {
+
+    private val inaiBackendOrdersUrl: String = BuildConfig.BaseUrl + "/orders"
+    private var orderId = ""
+    private val orderMetadata: Map<String, JsonPrimitive> = mutableMapOf(
+        "test_order_id" to JsonPrimitive("test_order")
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_buy.setOnClickListener {
+            prepareOrder()
+        }
+    }
+
+    private fun prepareOrder() {
+        showProgress()
+        val orderPostData = getDataForOrders()
+        val networkConfig = mutableMapOf(
+            NetworkRequestHandler.KEY_URL to inaiBackendOrdersUrl,
+            NetworkRequestHandler.KEY_REQUEST_TYPE to NetworkRequestHandler.POST,
+            NetworkRequestHandler.KEY_POST_DATA_JSON to Json.encodeToString(orderPostData)
+        )
+        makeNetworkRequest(networkConfig, ::onOrderPrepared)
+    }
+
+    private fun onOrderPrepared(orderResponse: String) {
+        hideProgress()
+        val orderResult = json.decodeFromString<OrderResult>(orderResponse)
+        Config.customerId = orderResult.customer_id
+        orderId = orderResult.id
+        addPaymentMethod()
+    }
+
+    private fun addPaymentMethod() {
+        if (BuildConfig.InaiToken.isNotEmpty() && orderId.isNotEmpty()) {
+            val config = InaiConfig(
+                token = BuildConfig.InaiToken,
+                orderId = orderId,
+                countryCode = Config.countryCode,
+                redirectUrl = ""
+            )
+            try {
+                val inaiCheckout = InaiCheckout(config)
+                inaiCheckout.addPaymentMethod(
+                    "card",
+                    requireContext(),
+                    this
+                )
+            } catch (ex: Exception) {
+                //  Handle initialisation error
+                requireActivity().showAlert("Error while initialising sdk : $ex.message")
+            }
+        }
+    }
+
+    override fun paymentMethodSaved(result: InaiPaymentMethodResult) {
+        when (result.status) {
+            InaiPaymentMethodStatus.Success -> {
+                requireActivity().showAlert("Payment Success! ${result.data}")
+                Log.d("Result", "${result.data}")
+            }
+            InaiPaymentMethodStatus.Failed -> {
+                requireActivity().showAlert("Payment Failed! ${result.data}")
+            }
+            InaiPaymentMethodStatus.Canceled -> {
+                var message = "Payment Canceled!"
+                if (result.data.has("message")) {
+                    message = result.data.getString("message")
+                }
+                requireActivity().showAlert(message)
+            }
+        }
+
+    }
+
+    /**
+     *  Helper Functions
+     */
+    private fun makeNetworkRequest(
+        networkConfig: Map<String, String>,
+        callback: (String) -> Unit
+    ) {
+        NetworkRequestHandler.makeNetworkRequest(networkConfig) { result: NetworkRequestHandler.Result ->
+            when (result) {
+                is NetworkRequestHandler.Result.Success -> {
+                    callback(result.message)
+                }
+                is NetworkRequestHandler.Result.Failure -> {
+                    onError(result.message)
+                }
+            }
+        }
+    }
+
+    private fun getDataForOrders(): OrderPostData {
+        return OrderPostData(
+            amount = Config.amount,
+            currency = Config.currency,
+            customer = OrderCustomer(
+                email = "customer@example.com",
+                first_name = "John",
+                last_name = "Doe",
+                contact_number = "01010101010",
+                id = Config.customerId
+            ),
+            metadata = JsonObject(orderMetadata)
+        )
+    }
+
+    private fun onError(error: String) {
+        hideProgress()
+        requireActivity().showAlert(error)
+    }
+
+    private fun showProgress() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgress() {
+        progressBar.visibility = View.INVISIBLE
+    }
+
+    /**
+     *  Fragment cycle callback.
+     *  Here we cancel coroutine scope which in turn cancels any ongoing network operations
+     */
+    override fun onStop() {
+        super.onStop()
+        NetworkRequestHandler.cancelCoroutineScope()
+        hideProgress()
+    }
+}
