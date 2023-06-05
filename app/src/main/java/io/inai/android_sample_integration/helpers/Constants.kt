2fc1994a073cc2f9086757c92a089b67385c7b92
inai-dinesh <dinesh@inai.io>
2023-06-05 20:45:14 +0530

    removed unused code

--- app/src/main/java/io/inai/android_sample_integration/helpers/Constants.kt
+++ app/src/main/java/io/inai/android_sample_integration/helpers/Constants.kt
@@ -2,9 +2,6 @@ package io.inai.android_sample_integration.helpers
 
 object Constants {
     const val CATEGORY_WALLET = "Wallet"
-    const val CATEGORY_UPI = "UPI"
-    const val RAIL_CODE_UPI = "upi"
-    const val MODE_CODE_UPI_INTENT = "upi_intent"
 
 
 }
